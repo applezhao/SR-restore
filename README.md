3d5c917e879778f50d5e62ad96c131ebb727f65d
applezhao <yanlizhao02@gmail.com>
2018-08-21 00:17:18 +0100

    change train.py to custom data
    change train.py to accept custom data

--- README.md
+++ README.md
@@ -9,57 +9,16 @@ Requires `tensorflow==1.1.0` and `keras==2.0.2`
     $ pip install -r ./requirements.txt
 
 
-## Super Resolution example
-
-````
-    # Resize low-res and high-res images for Super Resolution
-    $ python -m sr.images data/images/raw/ \
-        --lr data/images/lr \
-        --hr data/images/hr \
-        --lr_size 72x72 \
-        --hr_size 288x288
-
-    # Train Super Resolution model.
-    # Expects images in /path/images/lr/ and /path/images/hr/
-    $ python -m sr.train data/images \
-        --results results/ \
-        --network espcnn \
-        --scale 4 \
-        --epochs 100 \
-        --batch_size 32
-
-    # Predict high-res images using trained model weights.
-    # (Optional) use `--limit` option to limit num of images to perform on.
-    $ python -m sr.predict data/images/lr/ \
-        --output results/output/ \
-        --weights results/espcnn_20170701_190534/weights.h5 \
-        --network espcnn \
-        --scale 4 \
-        --batch_size 32 \
-        --limit 20
-````
-
 
 ## Image Restore example
 
 
 ````
-    # Resize a directory of images to single size
-    # We need /image_path/original/ and /image_path/enhanced/
-    $ python -m restore.images data/images/raw_original \
-        --output data/images/original \
-        --size 288x288
-
-    $ python -m restore.images data/images/raw_enhanced \
-        --output data/images/enhanced \
-        --size 288x288
-
 
     # Train Restore model.
     # Expects images in /path/images/original/ and /path/images/enhanced/
     # Saves model weights, checkpoints, and tensorboard logs to path specified in results.
-    $ python -m restore.train data/images \
-        --results results/ \
+    $ python -m restore.train 
         --network cnn \
         --epochs 100 \
         --batch_size 32
@@ -75,15 +34,3 @@ Requires `tensorflow==1.1.0` and `keras==2.0.2`
         --limit 20
 
 ````
-
-
-## Dataset
-
-celebA: (link)
-
-
-## References
-
-- Image Super Resolution (link)
-- Perceptual loss (link)
-- Efficient Real time convolution (link)
--- restore/train.py
+++ restore/train.py
@@ -1,17 +1,225 @@
 # -*- coding: utf-8 -*-
 
 from .models import restore_cnn_model, restore_cnn_bn_model
-from sr.train import Pipeline, get_image_shape, get_count, steps_for_batch_size, image_pair_generator, PSNRLoss
+from scipy.misc import imread
+##
+## Images
+##
+
+def getRGBAImgName(index, directory=r'/disk1/yanli-z/venv_rnn/experiment/data/commondata'):
+    prefix = str(index).zfill(4)
+    return os.path.join(directory, prefix+".rgba.tiff")
+
+def getAlbedoImgName(index, directory=r'/disk1/yanli-z/venv_rnn/experiment/data/commondata'):
+    prefix = str(index).zfill(4)
+    return os.path.join(directory, prefix+".albedo.tiff")
+
+def getMatteIDImgName(index, directory=r'/disk1/yanli-z/venv_rnn/experiment/data/commondata'):
+    prefix = str(index).zfill(4)
+    return os.path.join(directory, prefix+".matteid.tiff")
+
+def getFGDeepImgName(index, directory=r'/disk1/yanli-z/venv_rnn/experiment/data/fg'):
+    prefix = str(index).zfill(4)
+    return os.path.join(directory, r'data', prefix+".deep.tiff")
+
+# TODO : if we generate new mask, we need to redefine this function
+# now mask is as 0003.0.mask.tiff 0003.1.mask.tiff
+def getFGMaskImgName(index, subindex, directory=r'/disk1/yanli-z/venv_rnn/experiment/data/fg'):
+    prefix = str(index).zfill(4)
+    return os.path.join(directory, r'data', prefix+"."+str(subindex)+".mask.tiff")
+
+def getFGResultImgName(index, directory=r'/disk1/yanli-z/venv_rnn/experiment/data/fg'):
+    prefix = str(index).zfill(4)
+    return os.path.join(directory, r'result', prefix+".tiff")
+
+def list_filenames(indexRange, subindexRange, images_dir=r'/disk1/yanli-z/venv_rnn/experiment/data/fg'):
+    input_rgba_filepaths = []
+    input_albedo_filepaths = []
+    input_matteid_filepaths = []
+    input_deep_filepaths = []
+    input_mask_filepaths = []
+
+    output_filepaths = []
+    for index in range(indexRange[0], indexRange[1]+1):
+        rgbafilename = getRGBAImgName(index)
+        albedofilename = getAlbedoImgName(index)
+        matteidfilename = getMatteIDImgName(index)
+        deepfilename = getFGDeepImgName(index)
+        resultfilename = getFGResultImgName(index)
+        for subindex in range(subindexRange[0], subindexRange[1]+1):
+            maskfilename = getFGMaskImgName(index, subindex)
+
+            input_rgba_filepaths.append(rgbafilename)
+            input_albedo_filepaths.append(albedofilename)
+            input_matteid_filepaths.append(matteidfilename)
+            input_deep_filepaths.append(deepfilename)
+            input_mask_filepaths.append(maskfilename)
+
+            output_filepaths.append(resultfilename)
+
+    return input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths
+
+def get_filenames():
+    input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths = list_filenames([3,3], [0,1])
+    return input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths
+
+# TODO : change imread to tiffread
+def load_image(filename):
+    img = imread(filename)
+    return np.asarray(img, dtype=K.floatx())
+
+def get_images(filenames):
+    return np.asarray([load_image(f) for f in filenames])
+
+def get_image_shape():
+    input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths = get_filenames()
+    
+    rgbadata = load_image(input_rgba_filepaths[0])
+    albedodata = load_image(input_albedo_filepaths[0])
+    matteiddata = load_image(input_matteid_filepaths[0])
+    deepdata = load_image(input_deep_filepaths[0])
+    maskdata = load_image(input_mask_filepaths[0])
+    resultdata = load_image(output_filepaths[0])
+
+    # TODO : concat all input to x
+    x = rgbadata+albedodata+matteiddata+deepdata+maskdata
+    y = resultdata
+
+    return x.shape, y.shape
+
+def get_count():
+    input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths = get_filenames()
+    return len(input_rgba_filepaths)
+
+##
+## Iterator
+##
+
+def lr_hr_generator():
+    return image_pair_generator()
+
+def image_pair_generator():
+    input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths = get_filenames()
+    while 1:
+        for rgba, albedo, matteid, deep, mask, result in zip(input_rgba_filepaths, input_albedo_filepaths, input_matteid_filepaths, input_deep_filepaths, input_mask_filepaths, output_filepaths):
+            rgbadata = load_image(rgba)
+            albedodata = load_image(albedo)
+            matteiddata = load_image(matteid)
+            deepdata = load_image(deep)
+            maskdata = load_image(mask)
+            resultdata = load_image(result)
+
+            # TODO : concat all input to x
+            x = rgbadata+albedodata+matteiddata+deepdata+maskdata
+            y = resultdata
+
+            x = np.reshape(x, (1,) + x.shape)
+            y = np.reshape(y, (1,) + y.shape)
+            yield(x, y)
+
+
+def steps_for_batch_size( batch_size):
+    total = get_count()
+    return max(1, int(total/batch_size))
+
+##
+## Loss
+##
+
+def PSNRLoss(y_true, y_pred):
+    """
+    PSNR is Peek Signal to Noise Ratio, which is similar to mean squared error.
+
+    It can be calculated as
+    PSNR = 20 * log10(MAXp) - 10 * log10(MSE)
+
+    For images, MAXp = 255, so 1st term is 20 * log(255) == 48.1308036087.
+    """
+    def log10(x):
+        return K.log(x) / K.log(K.constant(10, dtype=K.floatx()))
+
+    return 48.1308036087 + -10. * log10(K.mean(K.square(y_pred - y_true)))
+
+class Pipeline():
+    def __init__(self, network='espcnn'):
+        self.network = network
+        self.results_root_dir = r'/disk1/yanli-z/venv_rnn/experiment/trainedresult'
+        self.results_dir = self._get_and_prepare_results_dir()
+
+    def _model_name(self):
+        # timestamp
+        ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+        model_name = "%s_%s" % (self.network, ts)
+        return model_name
+
+    def _get_and_prepare_results_dir(self):
+        """
+            results_dir
+                /model.h5
+                /weights.h5
+                /logs/
+                    ...
+        """
+        model_name = self._model_name()
+
+        # make output dirs
+        results_dir = "%s/%s/" % (self.results_root_dir, model_name)
+        mkdir_p(results_dir)
+        mkdir_p(results_dir + 'logs/')
+
+        print("\n\n[TRAIN]    saving results to %s\n" % results_dir)
+        return results_dir
+
+    def get_callbacks(self):
+        # callbacks -- tensorboard
+        log_dir = self.results_dir + 'logs/'
+        tensorboard = TensorBoard(log_dir=log_dir)
+
+        # callbacks -- model weights
+        weights_path = self.results_dir + 'weights.h5'
+        model_checkpoint = ModelCheckpoint(monitor='loss', filepath=weights_path, save_best_only=True)
+
+        # callbacks -- learning rate
+        reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, min_lr=1e-5)
+        return [tensorboard, model_checkpoint, reduce_lr]
+
+    def run(self, scale=4, epochs=100, batch_size=32, save=True):
+        # input shape
+        input_shape, output_shape = get_image_shape()
+        image_count = get_count()
+
+        print("[TRAIN] LR %s ==> HR %s. (%s images)" % (input_shape, output_shape, image_count))
+
+        # model
+        if (self.network == 'srcnn'):
+            model = create_srcnn_model(input_shape, scale=scale)
+        elif (self.network == 'resnet_up'):
+            model = create_resnet_up_model(input_shape, scale=scale)
+        elif (self.network == 'espcnn_bn'):
+            model = create_espcnn_bn_model(input_shape, scale=scale)
+        else:
+            model = create_espcnn_model(input_shape, scale=scale)
+
+        model.compile(loss='mse', optimizer=Adam(lr=1e-3), metrics=[PSNRLoss])
+
+        # callbacks
+        callbacks = self.get_callbacks()
+
+        # train
+        gen = image_pair_generator()
+        steps = steps_for_batch_size(batch_size)
+        model.fit_generator(gen, steps, epochs=epochs, callbacks=callbacks)
+
+        # save
+        if (save):
+            model_path = self.results_dir + "model.h5"
+            model.save(model_path)
 
 class RestorePipeline(Pipeline):
 
     def run(self, epochs=100, batch_size=32, save=True):
-        orig_path = self.root_dir + 'original/'
-        enh_path = self.root_dir + 'enhanced/'
-        input_shape = get_image_shape(orig_path)
-        output_shape = get_image_shape(enh_path)
-        image_count = get_count(orig_path)
-
+        input_shape, output_shape = get_image_shape()
+        image_count = get_count()
         print("[TRAIN] orig %s ==> enhanced %s. (%s images)" % (input_shape, output_shape, image_count))
 
         # model
@@ -26,8 +234,8 @@ class RestorePipeline(Pipeline):
         callbacks = self.get_callbacks()
 
         # train
-        gen = image_pair_generator(enh_path, orig_path)
-        steps = steps_for_batch_size(enh_path, batch_size)
+        gen = image_pair_generator()
+        steps = steps_for_batch_size(batch_size)
         model.fit_generator(gen, steps, epochs=epochs, callbacks=callbacks)
 
         # save
@@ -42,22 +250,22 @@ if __name__ == '__main__':
     import argparse
     import timeit
     parser = argparse.ArgumentParser(description="Train RESTORE model.")
-    parser.add_argument("image_path", type=str, help="Path to input images, expects sub-directories /path/original/ and /path/enhanced/.")
-    parser.add_argument("--results", type=str, default="results/restore/", help="Results base dir, will create subdirectories e.g. /results/model_timestamp/")
+    #parser.add_argument("image_path", type=str, help="Path to input images, expects sub-directories /path/original/ and /path/enhanced/.")
+    #parser.add_argument("--results", type=str, default="results/restore/", help="Results base dir, will create subdirectories e.g. /results/model_timestamp/")
     parser.add_argument("--network", type=str, default="cnn", help="Network architecture. Default=cnn")
     parser.add_argument("--epochs", type=int, default=100, help="Epochs. Default=100")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size. Default=32")
 
     args = parser.parse_args()
 
-    image_path = args.image_path
-    results_path = args.results
+    image_path = ''
+    results_path = ''
     network = args.network
     epochs = args.epochs
     batch_size = args.batch_size
 
     # training pipeline
-    p = RestorePipeline(image_path, results_path, network=network)
+    p = RestorePipeline(network=network)
 
     start_time = timeit.default_timer()
     p.run(epochs=epochs, batch_size=batch_size)
